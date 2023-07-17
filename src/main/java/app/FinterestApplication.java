f1f16901d9fb16597cd40da97eb90c8974273958
Maks Hrytsyna <hrytsynamax@gmail.com>
2023-07-17 23:09:51 +0300

    new docker config

--- src/main/java/app/FinterestApplication.java
+++ src/main/java/app/FinterestApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@org.springframework.boot.autoconfigure.EnableAutoConfiguration(
-        exclude = {org.springframework.boot.autoconfigure.web
-                .servlet.error.ErrorMvcAutoConfiguration.class})
+
 public class FinterestApplication {
     public static void main(String[] args) {
         SpringApplication.run(FinterestApplication.class, args);
