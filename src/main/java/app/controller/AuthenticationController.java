a9bca7feae293c1c7a397a8104d0e17791f46398
Maks Hrytsyna <hrytsynamax@gmail.com>
2023-07-20 13:49:12 +0300

    test deploy with rest controller

--- src/main/java/app/controller/AuthenticationController.java
+++ src/main/java/app/controller/AuthenticationController.java
@@ -9,12 +9,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@org.springframework.web.bind.annotation.RestController
+@Controller
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
     private final AuthenticationManager authenticationManager;
