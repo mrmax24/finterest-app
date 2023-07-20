9171e53555a079f6ece09fc9ba2b570860596465
Maks Hrytsyna <hrytsynamax@gmail.com>
2023-07-20 13:44:58 +0300

    test deploy with rest controller

--- src/main/java/app/controller/AuthenticationController.java
+++ src/main/java/app/controller/AuthenticationController.java
@@ -9,13 +9,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+@org.springframework.web.bind.annotation.RestController
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
     private final AuthenticationManager authenticationManager;
--- src/main/java/app/controller/IndexController.java
+++ src/main/java/app/controller/IndexController.java
@@ -4,15 +4,15 @@ import app.model.Account;
 import app.model.User;
 import app.service.AccountService;
 import app.service.UserService;
+import java.util.List;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@RestController
+@Controller
 public class IndexController {
     private final AccountService accountService;
     private final UserService userService;
@@ -23,7 +23,7 @@ public class IndexController {
     }
 
     @GetMapping("/")
-    public List<Account> index() {
+    public String index(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         org.springframework.security.core.userdetails.User principal =
                 (org.springframework.security.core.userdetails.User) auth.getPrincipal();
@@ -33,7 +33,7 @@ public class IndexController {
                         + " username: " + username));
 
         List<Account> accounts = accountService.getAllByUser(user.getId());
-        System.out.println("It is working!!!");
-        return accounts;
+        model.addAttribute("accounts", accounts);
+        return "redirect:/dashboard";
     }
 }
