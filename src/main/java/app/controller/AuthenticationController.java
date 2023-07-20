a0489e5a992752cad32529f160fca2f1f325c252
Maks Hrytsyna <hrytsynamax@gmail.com>
2023-07-20 13:50:58 +0300

    test deploy with rest controller

--- src/main/java/app/controller/AuthenticationController.java
+++ src/main/java/app/controller/AuthenticationController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@org.springframework.web.bind.annotation.RestController
+@Controller
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
     private final AuthenticationManager authenticationManager;
